30f5da2740579c0f28875e4374913b22703647d3
user <1403000890@qq.com>
2021-02-02 17:21:36 +0800

    Modification Intermediaires

--- src/Controller/ServeeurController.php
+++ src/Controller/ServeeurController.php
@@ -17,4 +17,35 @@ class ServeeurController extends AbstractController
             'controller_name' => 'ServeeurController',
         ]);
     }
+	/**
+     * @Route("/form", name="form")
+     */
+    public function form(): Response
+    {
+        return $this->render('serveeur/index.html.twig', [
+            'controller_name' => 'ServeeurController',
+        ]);
+    }
+	    /**
+     * @Route("/reset", name="reset")
+     */
+    public function resete(EntityManagerInterface $manager): Response
+    {
+		$manager -> getRepository(Acces::class)->vider();
+		$manager -> getRepository(Acces::class)->vider();
+		$manager -> getRepository(Acces::class)->vider();
+		$manager -> getRepository(Acces::class)->vider();
+        return $this->render('serveeur/index.html.twig', [
+            'controller_name' => 'ServeeurController',
+        ]);
+    }    
+	/**
+     * @Route("/creerUtilisateur", name="creerUtilisateur")
+     */
+    public function creerUtilisateur(): Response
+    {
+        return $this->render('serveeur/index.html.twig', [
+            'controller_name' => 'ServeeurController',
+        ]);
+    }
 }
